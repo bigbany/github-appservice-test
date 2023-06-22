56a6223da7c034a7aafbf3259416e0389a0f60db
imnotdorai <63151655+bigbany@users.noreply.github.com>
2023-06-22 15:13:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():    
-    return "<a href='https://facebook.com' target='_blank'>페이스북</a>"
+    return "<a href='https://facebook.com' target='_blank'>페이스북 테스트 2이다. </a>"
 
 @app.route('/watch')
 def home2(): 
